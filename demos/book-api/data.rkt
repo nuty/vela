723f03ec040718ee861ef84c909a4e7edddef74b
jooojoe <wuweiru@apluslabs.com>
2019-04-29 20:57:58 +0800

    code clean

--- demos/book-api/data.rkt
+++ demos/book-api/data.rkt
@@ -9,13 +9,3 @@
     (hasheq 'id 5 'title "Head First Ruby: A Brain-Friendly Guide" 'author "Jay McGavren")))
 
 (provide books)
-
-
-; (require net/url)
-; (require json)
-; (define (get-json url)
-;    (call/input-url (string->url url)
-;                    get-pure-port
-;                    (compose string->jsexpr port->string)))
-; ;; Returns a parsed JSON expression
-; (get-json "https://www.ifixit.com/api/2.0/guides/13470")
