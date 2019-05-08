15f5949150c924886ba02e5498596a3211de8745
Rosso Wu <jooojoe@126.com>
2019-05-08 15:02:53 +0800

    update demo

--- main.rkt
+++ main.rkt
@@ -133,7 +133,11 @@
      [file-path (rest (string-split req-full-path static-url))]
      [file-type (last (string-split req-full-path "."))]
      [full-file-path (build-path static-path (substring (car file-path) 1))])
-      (response/file full-file-path file-type)))
+
+      (cond 
+        [(file-exists? full-file-path) (response/file full-file-path file-type)]
+        [else (not-found req)]
+        )))
 
 
 (define (route->handler req routers keys static-path static-url)
