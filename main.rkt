64f0de2275173be7e5bbb43dc9828255420e8e94
jooojoe <wuweiru@apluslabs.com>
2019-12-23 10:24:11 +0800

    expose

--- main.rkt
+++ main.rkt
@@ -68,21 +68,21 @@
 
 
 (define MIME-TYPE-HASH
-    (hash
-      "jpg" #"image/jpg; charset=utf-8"
-      "jpeg" #"image/jpeg; charset=utf-8"
-      "png" #"image/png; charset=utf-8"
-      "gif" #"image/gif; charset=utf-8"
-      "js" #"application/x-javascript; charset=utf-8"
-      "xml" #"application/xml; charset=utf-8"
-      "json" #"application/json; charset=utf-8"
-      "css" #"text/css; charset=utf-8"
-      "doc" #"application/msword; charset=utf-8"
-      "docx" #"application/msword; charset=utf-8"
-      "xls" #"application/excel; charset=utf-8"
-      "pdf" #"image/pdf; charset=utf-8"
-      "mp4" #"video/mpeg4; charset=utf-8"
-      "mp3" #"audio/mp3; charset=utf-8"))
+  (hash
+    "jpg" #"image/jpg; charset=utf-8"
+    "jpeg" #"image/jpeg; charset=utf-8"
+    "png" #"image/png; charset=utf-8"
+    "gif" #"image/gif; charset=utf-8"
+    "js" #"application/x-javascript; charset=utf-8"
+    "xml" #"application/xml; charset=utf-8"
+    "json" #"application/json; charset=utf-8"
+    "css" #"text/css; charset=utf-8"
+    "doc" #"application/msword; charset=utf-8"
+    "docx" #"application/msword; charset=utf-8"
+    "xls" #"application/excel; charset=utf-8"
+    "pdf" #"image/pdf; charset=utf-8"
+    "mp4" #"video/mpeg4; charset=utf-8"
+    "mp3" #"audio/mp3; charset=utf-8"))
 
 (define (not-found req)
   (response
@@ -252,4 +252,5 @@
   not-found
   url-group
   render
+  response
   jsonify)
