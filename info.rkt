5a75da5b8d3487a4af45738dc2e928964668e645
rosso <heizai3@gmail.com>
2020-02-22 19:17:07 +0800

    middleware

--- info.rkt
+++
@@ -1,6 +0,0 @@
-#lang info
-(define collection "vela")
-(define deps '("web-server"))
-(define pkg-desc "Simple web framework to build restful app in Racket. ")
-(define version "0.1")
-(define pkg-authors '(rosso))
