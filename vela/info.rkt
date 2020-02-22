6df15009db91d41a71bb0720911f3d0907d3fa6b
rosso <heizai3@gmail.com>
2020-02-23 01:06:38 +0800

    update package-name

--- vela/info.rkt
+++ vela/info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection 'multi)
-(define deps'("vela-lib" "vela2-doc" "base"))
+(define deps'("vela-lib" "base"))
 (define implies
-  '("vela-lib" "vela2-doc"))
+  '("vela-lib"))
 (define pkg-desc "Simple web framework to build restful app in Racket. ")
 (define version "0.2")
 (define pkg-authors '(rosso))
